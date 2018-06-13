7b560c3c10c8e20b8f8ed15a7ca3664edc10a779
Ula Walińska <akane96@onet.pl>
2018-06-13 20:53:26 +0200

    a little of cleaning stuff

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //StoredField idFiled = new StorageField(Constants.id, id);
+        //...htmlFile(file)
+        //...xtFiled(Constants.content, content, Filed.Store.NO);
+        //w indexerze
+
+        //StoredFiled(Constants.filesize, (int)file) --> bo nie indexowany i przechowywany
+    }
+}
